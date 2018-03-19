9bff69d056ff38ab3a6e14e514b1fc2e210ed981
So <quddbs30@naver.com>
2018-03-20 00:05:37 +0900

    add

--- Chap4/SelfRef.cpp
+++ Chap4/SelfRef.cpp
@@ -12,7 +12,9 @@ class SelfRef
         cout << "객체생성" << endl;
     }
     /************
-     *  by07.so TODO 참조자형 반환을 위해 this가 아닌 *this 를 사용하는 이유???
+     * by07.so TODO 참조자형 반환을 위해 this가 아닌 *this 를 사용하는 이유???
+     * Answer : (non-const)참조자형 변수는 lvalue 로서, 변경 가능한 변수를 참조해야 하기 때문.
+     *          *this 를 반환해야 변경 가능한 변수의 '참조 정보'를 할당 받을 수 있다.
     *****/ 
     SelfRef &Adder(int n)
     {
@@ -24,6 +26,8 @@ class SelfRef
         cout << num << endl;
         return *this;
     }
+
+    SelfRef &myRef = *this;
 };
 
 int main(void)
