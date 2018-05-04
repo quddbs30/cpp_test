5555e1a64ff9f828c61e65c324f3bfb69c13a0ae
So <quddbs30@naver.com>
2018-05-04 23:30:10 +0900

    test of find() for list

---
+++ Test/list_test.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <list>
+#include <cstring>
+using namespace std;
+
+typedef struct t_data
+{
+    char name[10];
+    int age;
+
+    bool operator==(const struct t_data &other)
+    {
+        if ((0 == strcmp(name, other.name)) &&
+            age == other.age)
+            return true;
+        else
+            return false;
+    }
+} tData;
+
+int main(void)
+{
+    list<tData> lTemp;
+
+    tData d1 = {"d1", 1};
+    tData d2 = {"d2", 2};
+    tData d3 = {"d3", 3};
+
+    lTemp.push_back(d1);
+    lTemp.push_back(d2);
+    lTemp.push_back(d3);
+
+    tData tt = {"d2", 2};
+
+    /*********************
+     * by07.so TIP List 에서 특정 원소를 탐색할 때 아래와 같이 하면 된다.
+     *******************/
+    if (find(lTemp.begin(), lTemp.end(), tt) != lTemp.end())
+        cout << "got it " << endl;
+    else
+        cout << "not saved " << endl;
+
+    return 0;
+}
